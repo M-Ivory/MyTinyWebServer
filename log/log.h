c8b8155333507726649eeed30fd1dd5b267a1e8d
ivory <1205822960@qq.com>
2024-07-08 01:31:16 +0800

    7.8 1:31

---
+++ log/log.h
@@ -0,0 +1,57 @@
+//
+// Created by ivory on 24-7-7.
+//
+
+#ifndef LOG_H
+#define LOG_H
+#include <iostream>
+#include <string>
+#include <pthread.h>
+#include "block_queue.h"
+
+using namespace std;
+class Log {
+public:
+    static Log* get_instance() {// 此函数用于获取一个Log实例，因为Log的构造函数为private
+        static Log instance;
+        return &instance;
+    }
+    static void* flush_log_thread(void* args) {// 调用private写日志函数，执行异步日志写操作
+        Log::get_instance()->async_write_log();
+    }
+    bool init(const char* file_name, int close_log, int log_buf_size = 8192, int split_lines = 5000000, int max_queue_size = 0);
+    void write_log(int level, const char* format, ...);
+    void flush(void);
+private:
+    // 构造函数和析构函数私有化可以
+    // 构造函数私有化：防止在类的外部通过 new 或者其他方式创建实例。
+    // 析构函数私有化：防止在类的外部通过 delete 直接销毁实例。
+    Log();
+    virtual ~Log();
+    // virtual的作用是，倘若有个子类继承Log，delete删除子类对象时，
+    // 若无virtual，则会只调用父类中的析构函数～Log，而不会调用子类的析构函数
+    // 若有virtual，则会先调用子类的析构函数，后调用父类中的析构函数～Log
+    void* async_write_log() {// 此函数将队列中一条日志写入文件
+        string single_log;
+        while(my_log_queue->pop(single_log)) {
+            my_lock.lock();
+            fputs(single_log.c_str(), my_log_fp);
+            my_lock.unlock();
+        }
+    }
+private:
+    char dir_name[128];// 路径名
+    char log_name[128];// 日志名
+    int my_log_max_lines;// 日志最多行数
+    int my_log_buf_size;// 日志缓冲区大小
+    long long my_log_count;// 记录日志行数
+    int my_today;// 记录当天
+    FILE *my_log_fp;// 打开log的文件指针
+    char *my_log_buf;// 日志缓冲区
+    block_queue<string> *my_log_queue;// 日志队列
+    // 此处是类的指针，而队列在类中；是用指针应该是为了减少开销？
+    bool is_async;// 是否异步标志
+    locker my_lock;// 互斥锁
+    int my_close_log;// 关闭日志
+};
+#endif //LOG_H
