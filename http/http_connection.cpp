d1d813374b98866c45ef78dd3684cdd945a76877
ivory <1205822960@qq.com>
2024-08-19 22:11:19 +0800

    8.19 22:11

--- http/http_connection.cpp
+++ http/http_connection.cpp
@@ -4,11 +4,116 @@
 
 #include "http_connection.h"
 
-void http_connection::init(int sockfd, const sockaddr_in &addr, char *, int, int, string user, string passwd,
-    string sqlname) {
+#include <cstring>
+#include <fcntl.h>
+#include <mysql/mysql.h>
+#include <fstream>
+
+//定义http响应的一些状态信息
+const char *ok_200_title = "OK";
+const char *error_400_title = "Bad Request";
+const char *error_400_form = "Your request has bad syntax or is inherently impossible to staisfy.\n";
+const char *error_403_title = "Forbidden";
+const char *error_403_form = "You do not have permission to get file form this server.\n";
+const char *error_404_title = "Not Found";
+const char *error_404_form = "The requested file was not found on this server.\n";
+const char *error_500_title = "Internal Error";
+const char *error_500_form = "There was an unusual problem serving the request file.\n";
+
+locker my_lock;// 互斥锁
+map<string, string> users;// sql用户名密码
+
+// 查询所有用户名和密码映射并存在全局变量users中
+void http_connection::initmysql_result(connection_pool *connPool) {
+    MYSQL* mysql = NULL;
+    connectionRAII mysqlcon(&mysql, connPool);// 创建一个sql连接类，这个连接销毁时自动释放
+    if(mysql_query(mysql, "SELECT username,passwd FROM user"))// 执行一条 MySQL 查询
+        LOG_ERROR("SELECT error:%s\n", mysql_error(mysql));
+    MYSQL_RES* result = mysql_store_result(mysql);// 查询结果
+    int num_fields = mysql_num_fields(result);// 查询结果列数
+    MYSQL_FIELD* fields = mysql_fetch_field(result);// 查询结果结构数组
+    while(MYSQL_ROW row = mysql_fetch_row(result)) {
+        // row为查询结果中每一行的数据
+        string temp1(row[0]);
+        string temp2(row[1]);
+        users[temp1] = temp2;// 存储用户名密码的映射
+
+    }
 }
 
+// 对文件描述符设置非阻塞
+// 可见lst_timer.cpp
+int setnonblocking(int fd) {
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+// 添加文件描述符到epoll中
+void addfd(int epollfd, int fd, bool one_shot, int TRIGMode) {
+    epoll_event event;
+    event.data.fd = fd;
+    if(TRIGMode == 1) event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+    else event.events = EPOLLIN | EPOLLRDHUP;
+    // EPOLLIN: 读事件。
+    // EPOLLET: 边缘触发模式。
+    // EPOLLRDHUP: 对端关闭连接。
+    // 每一个宏定义占一位，用或运算，最后结果哪一位为1,则表示用了哪一位
+    if(one_shot)event.events |= EPOLLONESHOT;
+    // EPOLLONESHOT: 事件只触发一次，避免同一个文件描述符被多个线程处理
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    // 将文件描述符 fd 添加到 epoll 实例 epollfd 中，并注册事件
+    setnonblocking(fd);  // 设置文件描述符为非阻塞模式
+}
+
+// 从epoll中移除文件描述符
+void removefd(int epollfd, int fd)
+{
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+    close(fd);
+}
+
+// 修改epoll中文件描述符的事件
+void modfd(int epollfd, int fd, int ev, int TRIGMode) {
+    // ev为新的事件类型，可能为EPOLLIN、EPOLLOUT等
+    epoll_event event;
+    event.data.fd = fd;
+    if(TRIGMode == 1) event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP;
+    else event.events = ev | EPOLLONESHOT | EPOLLRDHUP;
+    // EPOLLIN: 读事件。
+    // EPOLLET: 边缘触发模式。
+    // EPOLLRDHUP: 对端关闭连接。
+    // 每一个宏定义占一位，用或运算，最后结果哪一位为1,则表示用了哪一位
+    epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
+    // EPOLL_CTL_MOD: 修改已注册的文件描述符事件
+}
+
+int http_connection::my_user_count = 0;
+int http_connection::my_epollfd = -1;
+
+// 初始化连接
+void http_connection::init(int sockfd, const sockaddr_in &addr, char *root,
+    int TRIGMode, int close_log, string user, string passwd, string sqlname) {
+    my_sockfd = sockfd;// 客户端socket套接字
+    my_address = addr;// 客户端地址
+    addfd(my_epollfd, sockfd, true, my_trig_mode);// 将客户端socket添加到epoll实例中
+    my_user_count++;// 连接数+1
+    doc_root = root;// 设置服务器根目录
+    strcpy(sql_user, user.c_str());
+    strcpy(sql_passwd, passwd.c_str());
+    strcpy(sql_name, sqlname.c_str());
+    init();
+}
+
+// 关闭连接，关闭一个连接，客户总量减一
 void http_connection::close_conn(bool real_close) {
+    if(real_close && my_sockfd != -1) {
+        printf("close %d\n", my_sockfd);
+        removefd(my_epollfd, my_sockfd);
+        my_sockfd = -1;
+        my_user_count--;
+    }
 }
 
 void http_connection::process() {
@@ -20,8 +125,7 @@ bool http_connection::read_once() {
 bool http_connection::write() {
 }
 
-void http_connection::initmysql_result(connection_pool *connPool) {
-}
+
 
 void http_connection::init() {
 }
--- http/http_connection.h
+++ http/http_connection.h
@@ -9,6 +9,8 @@
 #include <sys/stat.h>
 #include <map>
 #include <sys/uio.h>
+#include <sys/epoll.h>
+#include <unistd.h>
 #include "../lock/lockDefine.h"
 #include "../mysql_pool/sql_connection_pool.h"
 // #include "../timer/lst_timer.h"
@@ -16,14 +18,14 @@
 
 class http_connection {
 public:
-    static const int FILENAME_LENGTH = 200;
-    static const int READ_BUFF_SIZE = 2048;
-    static const int WRITE_BUFF_SIZE = 1024;
-    enum METHOD{GET, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT, PATH};
-    enum CHECK_STATE{REQUEST_LINE, HEADER, CONTENT};
+    static const int FILENAME_LENGTH = 200;// 文件最大长度
+    static const int READ_BUFF_SIZE = 2048;// 读缓冲区
+    static const int WRITE_BUFF_SIZE = 1024;// 写缓冲区
+    enum METHOD{GET, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT, PATH};//http请求类型
+    enum CHECK_STATE{REQUEST_LINE, HEADER, CONTENT};// 主状态机的状态
     enum HTTP_STATE{NO_REQUEST, GET_REQUEST, BAD_REQUEST, NO_RESOURCE,
         FORBIDDEN_REQUEST, FILE_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION};// 连接状态，例如200、404
-    enum LINE_STATUS{LINE_OK, LINE_BAD, LINE_OPEN};
+    enum LINE_STATUS{LINE_OK, LINE_BAD, LINE_OPEN};// 读取行状态
 public:
     http_connection()= default;
     ~http_connection()= default;
@@ -61,48 +63,50 @@ private:
     bool add_blank_line();
 
 public:
-    static int my_epollfd;//
-    static int my_user_count;
-    MYSQL* mysql;
+    static int my_epollfd;// epoll描述符
+    static int my_user_count;// 当前连接数
+    MYSQL* mysql;// 数据库连接
     int my_state;// 事件类型，0为读，1为写
 
 private:
-    int my_sockfd;// 
+    int my_sockfd;// 客户端套接字文件描述符
+    // 如：IP：Port
     sockaddr_in my_address;// 表示 IPv4 网络地址
-    char my_read_buf[READ_BUFF_SIZE];
+    char my_read_buf[READ_BUFF_SIZE];// 读缓冲区，用于存放客户端请求数据
     long my_read_buf_index;// 读缓冲的指示器
-    char my_write_buf[WRITE_BUFF_SIZE];
+    char my_write_buf[WRITE_BUFF_SIZE];// 写缓冲区，用于存放服务器响应数据。
     long my_write_buf_index;// 读缓冲的指示器
-    long my_checked_idx;// 
-    int my_start_line;//
-    CHECK_STATE my_check_state;// 
-    METHOD my_method;//
+    long my_checked_idx;// 当前解析位置的下标
+    int my_start_line;// 每行数据的起始位置
+    CHECK_STATE my_check_state;// 当前主状态机的状态
+    METHOD my_method;// 当前http请求方法
     char my_real_file[FILENAME_LENGTH];// 真实文件名是去除路径的文件名or绝对路径文件名？
-    char *my_url;//
-    char *my_version;//
-    char *my_host;//
+    char *my_url;// 请求目标url
+    char *my_version;// http版本
+    char *my_host;// host字段
     long my_content_length;// 内容长度
-    bool my_linger;//
-    char *my_file_address;// 文件路径
-    struct stat my_file_stat;// 获取文件的状态信息
+    bool my_linger;// 表示http的连接状态
+    char *my_file_address;// 文件的内存映射地址
+    struct stat my_file_stat;// 请求文件的状态信息
     struct iovec my_iv[2];// 向量 I/O 操作中的缓冲区
+    // 可以分散读和集中写
     // 向量 I/O 操作是一种高效的数据传输方式，可以将多个缓冲区的数据一次性地进行读写操作
     // iov_base：指向缓冲区的起始地址。
     // iov_len：表示缓冲区的长度。
-    int my_iv_count;//
-    int cgi;        //是否启用的POST
-    char *my_string; //存储请求头数据
-    int bytes_to_send;//
-    int bytes_have_send;//
-    char *doc_root;
+    int my_iv_count;// ivoec结构体的数量
+    int cgi;// 表示是否启用了CGI
+    char *my_string; // 存储post请求的请求体数据
+    int bytes_to_send;// 要发送的字节数
+    int bytes_have_send;// 已经发送的字节数
+    char *doc_root;// 服务器根目录
 
-    map<string, string> my_users;
-    int my_trig_mode;// 触发器模式
+    map<string, string> my_users;// 用户名和密码的数据库映射
+    int my_trig_mode;// 触发器模式，分LT和ET
     int my_close_log;// 日志开关
 
-    char sql_user[100];
-    char sql_passwd[100];
-    char sql_name[100];
+    char sql_user[100];// 数据库用户名
+    char sql_passwd[100];// 数据库密码
+    char sql_name[100];// 数据库名
 };
 
 #endif //HTTP_CONNECTION_H
